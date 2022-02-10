ab3b485014cf572050dd899d61f0071ed49af775
IsidoraStanojevic <isidora0707@gmail.com>
2022-02-10 19:43:49 +0100

    Database import

--- 02resources/petAdoption.sql
+++ 02resources/petAdoption.sql
@@ -5,11 +5,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS `petadoption`;
 CREATE DATABASE IF NOT EXISTS `petadoption` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `petadoption`;
 
-DROP TABLE IF EXISTS `ad`;
 CREATE TABLE IF NOT EXISTS `ad` (
   `ad_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -18,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `ad` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `species_id` int(10) unsigned DEFAULT NULL,
   `breed` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `papers` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `has_papers` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `age` int(3) unsigned NOT NULL,
-  `vaccinated` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `is_vaccinated` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `color_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`ad_id`),
   KEY `fk_ad_color_id` (`color_id`),
@@ -29,46 +27,45 @@ CREATE TABLE IF NOT EXISTS `ad` (
   CONSTRAINT `fk_ad_color_id` FOREIGN KEY (`color_id`) REFERENCES `color` (`color_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_ad_species_id` FOREIGN KEY (`species_id`) REFERENCES `species` (`species_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_ad_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `color`;
 CREATE TABLE IF NOT EXISTS `color` (
   `color_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `color_name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`color_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`color_id`),
+  UNIQUE KEY `uq_color_color_name` (`color_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `photo`;
 CREATE TABLE IF NOT EXISTS `photo` (
   `photo_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `image_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ad_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`photo_id`),
+  UNIQUE KEY `uq_photo_image_path` (`image_path`),
   KEY `fk_photo_ad_id` (`ad_id`),
   CONSTRAINT `fk_photo_ad_id` FOREIGN KEY (`ad_id`) REFERENCES `ad` (`ad_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `species`;
 CREATE TABLE IF NOT EXISTS `species` (
   `species_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`species_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`species_id`),
+  UNIQUE KEY `uq_species_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pasword_hash` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pasword_reset_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pasword_reset_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `forname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `surname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phonenumber` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `postal_adress` text COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `uq_user_email` (`email`),
-  UNIQUE KEY `uq_user_phonenumber` (`phonenumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  UNIQUE KEY `uq_user_phone_number` (`phone_number`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
